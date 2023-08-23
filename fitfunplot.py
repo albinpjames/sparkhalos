corrected normalisation

fitfunplot.py
@@ -64,63 +64,75 @@ plt.suptitle("CIC From Field Particles", fontsize=15, y=0.95)
 
 for p, nw_boxsize in enumerate(nw_boxsizes): 
 	print(f"Calculating for boxsize {nw_boxsize}")
-	boxdata = np.load(datalocation + "/" + str(nw_boxsize) +".npy")
+	particles_in_box = np.load(datalocation + "/" + str(nw_boxsize) +".npy")
 
-	cell_avg = np.sum(boxdata) * (nw_boxsize**3) / (size**3)
-	celldensity = (boxdata - cell_avg)/cell_avg
-	oldboxdata = boxdata
-	boxdata = celldensity
+	
+	'Here we are converting the particles in box to density contrast '
+	cell_avg = np.sum(particles_in_box) * (nw_boxsize**3) / (size**3)
+	celldensity = (particles_in_box - cell_avg)/cell_avg
+	boxdata = celldensity # The density contrast
+	boxdata = np.log(boxdata+1)
 
-
-    # Plotting the data
-    
+	'Plotting the data'
 	ax = plt.subplot(nrows, ncols, p+1)
-	y_value, binedges, patches = ax.hist(boxdata, bins = cicbins, density=True, facecolor = '#2ab0ff', edgecolor='#169acf', linewidth=0.5, alpha = 0.5)
-	x_value = (0.5*(binedges[1:] + binedges[:-1]))
-	x_value = np.log(x_value+1)
-
 	ax.set_title(f"CIC Computed for {nw_boxsize} sub box size")
+	# ax.set_xscale('log')
 
+	'The data is binned and x and y values are obtained'
+	hist_y, hist_edge = np.histogram(boxdata, bins=cicbins)
 
-	# Calculating the error 
-	y_valueerr, binedgeserr = np.histogram(boxdata, bins=cicbins)
-	errorbar = np.sqrt(y_valueerr)/((binedges[1]-binedges[0])*np.sum(y_valueerr))
+	normfactor = ((hist_edge[1]-hist_edge[0])*np.sum(hist_y))
+	y_value = hist_y / normfactor
+	
+	'The delta is converted to log(delta+1)'
+	x_center = (0.5*(hist_edge[1:] + hist_edge[:-1]))
+	# x_value = np.log(x_center+1)
+	x_value = x_center
+
+	# ax.scatter(x_value,y_value)
+
+	'Error'
+	errorbar = np.sqrt(hist_y) / normfactor
 	ax.errorbar(x_value, y_value, yerr=errorbar, fmt='k.')
 
-
-	# Curve Fitting GEV SciP
-	print(f"Calculating fitting for Genextreme {nw_boxsize}")
-	# popt_genex = genextreme.fit(x_value)
-	popt_genex, pcov_gev = curve_fit(genextreme.pdf, x_value, y_value, p0 =(skew(boxdata),np.mean(boxdata),np.std(boxdata)))
-	xi, nu_g, sig_g = popt_genex
-	print(f"Plotting Genextreme {nw_boxsize}")
-	ax.plot(x_value, genextreme.pdf(x_value, - xi, nu_g, sig_g), 'b--', label='Genextreme (Fit): \nxi=%5.3f, \nnu_g=%5.3f, \nsig_g=%5.3f' % tuple(popt_genex))
-
-
-	# Curve Fitting GEV
+		# Curve Fitting GEV
 	print(f"Calculating fitting for GEV {nw_boxsize}")
-	popt_gev, pcov_gev = curve_fit(gev, x_value, y_value, p0 =(skew(boxdata),np.mean(boxdata),np.std(boxdata)))
+	popt_gev, pcov_gev = curve_fit(gev, x_value, y_value, 
+		p0 =(skew(boxdata),np.mean(boxdata),np.std(boxdata))
+		)
 	print(f"Plotting GEV {nw_boxsize}")
-	ax.plot(x_value, gev(x_value, *popt_gev), 'g--', label='GEV (Fit): \nxi=%5.3f, \nnu_g=%5.3f, \nsig_g=%5.3f' % tuple(popt_gev))
+	# xvalue = np.arange(-1,1,0.1 )
+	xvalue = x_value
+	ax.plot(xvalue, gev(xvalue, *popt_gev), 'g--', label='GEV (Fit): \nxi=%5.3f, \nnu_g=%5.3f, \nsig_g=%5.3f' % tuple(popt_gev))
+
+	# # Curve Fitting GEV SciP
+	# print(f"Calculating fitting for Genextreme {nw_boxsize}")
+	# # popt_genex = genextreme.fit(x_value)
+	# popt_genex, pcov_gev = curve_fit(genextreme.pdf, x_value, y_value, p0 =(skew(boxdata),np.mean(boxdata),np.std(boxdata)))
+	# xi, nu_g, sig_g = popt_genex
+	# print(f"Plotting Genextreme {nw_boxsize}")
+	# ax.plot(x_value, genextreme.pdf(x_value, - xi, nu_g, sig_g), 'b--', label='Genextreme (Fit): \nxi=%5.3f, \nnu_g=%5.3f, \nsig_g=%5.3f' % tuple(popt_genex))
+
 
     # Curve Fitting ln Norm
 	print(f"Calculating fitting for Log Normal {nw_boxsize}")
 	popt_lnnrm, pcov_lnnrm = curve_fit(lnnorm, x_value, y_value, p0 =(np.mean(boxdata),np.std(boxdata)))
 	print(f"Plotting Log Normal {nw_boxsize}")
-	ax.plot(x_value, lnnorm(x_value, *popt_lnnrm), 'r--', label='GEV (Fit): \nnu =%5.3f, \nsig =%5.3f' % tuple(popt_lnnrm))
+	ax.plot(x_value, lnnorm(x_value, *popt_lnnrm), 'r--', label='Lognorm (Fit): \nnu =%5.3f, \nsig =%5.3f' % tuple(popt_lnnrm))
 	
 	# xvalue = np.arange(-1,4,0.1 )
 	# ax.plot(xvalue, genextreme.pdf(xvalue,-0.5,0,0.4), label='scipy genextreme')
 	# ax.plot(xvalue, gev(xvalue, 0.5,0,0.4), label='gev')
+
 
 	# Curve Fitting Poisson
 	# popt_pois, pcov_pois = curve_fit(pois, x_value, y_value, p0=(np.mean(boxdata),))
 	# ax.plot(x_value, pois(x_value, np.mean(boxdata)), 'b--', label=f'Mean (Poisson Fit):{popt_pois} \n Actual Mean {np.mean(boxdata)}' )
 
 	# Curve Fitting Normal Dist
-	# popt_norm, pcov_norm = curve_fit(normfun, x_value, y_value, p0 = (np.mean(boxdata), np.std(boxdata)) )
-	# ax.plot(x_value, normfun(x_value, *popt_norm), 'g-', label='Normal (Fit): \nn=%5.3f, sig=%5.3f' % tuple(popt_norm))
+	popt_norm, pcov_norm = curve_fit(normfun, x_value, y_value, p0 = (np.mean(boxdata), np.std(boxdata)) )
+	ax.plot(x_value, normfun(x_value, *popt_norm), 'b--', label='Normal (Fit): \nn=%5.3f, sig=%5.3f' % tuple(popt_norm))
 
-	ax.legend(loc="upper right", fontsize=5)
+	ax.legend(loc="upper right", fontsize=4)
 
 plt.show()